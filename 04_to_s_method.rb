ee1af86f92c579e1d4b97ab76db16f7fdc67ad39
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-01 13:23:10 +0000

    add more examples with comments

---
+++ 04_to_s_method.rb
@@ -0,0 +1,21 @@
+class GoodDog
+  DOG_YEARS = 7
+
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    self.name = n
+    self.age  = a * DOG_YEARS
+  end
+
+  def to_s
+    "This dog's name is #{name} and it is #{age} in dog years."
+  end
+end
+
+sparky = GoodDog.new("Sparky", 4)
+
+puts sparky #puts is same thing as defauly Ruby method to_s which can be overwritten
+p sparky # same thing as puts sparky.inspect
+
+puts "#{sparky}"
