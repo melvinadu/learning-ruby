1aca7fde5f68e6e3aceaea6c5012e18c57126238
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-01 15:41:23 +0000

    add namespacing example with comments

--- 08_method_lookup_path.rb
+++ 08_method_lookup_path.rb
@@ -42,3 +42,24 @@ end
 
 puts "---GoodDog method lookup---"
 puts GoodDog.ancestors
+
+module Mammal
+  class Dog
+    def speak(sound)
+      p "#{sound}"
+    end
+  end
+
+  class Cat
+    def say_name(name)
+      p "#{name}"
+    end
+  end
+
+  
+end
+
+buddy = Mammal::Dog.new
+kitty = Mammal::Cat.new
+buddy.speak('Arf!')           # => "Arf!"
+kitty.say_name('kitty')       # => "kitty"
