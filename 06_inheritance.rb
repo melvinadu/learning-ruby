f8efd20fc6c0228d3ed50ab4b0e73907a1fb686c
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-01 14:18:22 +0000

    add superclass example with comments

--- 06_inheritance.rb
+++ 06_inheritance.rb
@@ -5,21 +5,26 @@ class Animal
 end
 
 class GoodDog < Animal
-  attr_accessor :name
+  # attr_accessor :name
 
-  def initialize(n)
-    self.name = n
-  end
+  # def initialize(n)
+  #   self.name = n
+  # end
+
+  # def speak
+  #   "#{self.name} says arf!"
+  # end
 
   def speak
-    "#{self.name} says arf!"
+    super + " called from GoodDog class using super keyword to pull method from Animal superclass"
   end
+
 end
 
 class Cat < Animal
 end
 
-sparky = GoodDog.new("Sparky") #we're overriding the speak method in the Animal class because Ruby checks the object's class first for the method before it looks in the superclass.
+sparky = GoodDog.new
 
 paws = Cat.new
 
